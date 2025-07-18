4b66996997facc7f7144f56c5b8f6630fd6237de
ifallious <ifallious@gmail.com>
2025-07-18 16:09:34 +0200

    kms

--- src/components/ItemDetailsModal.tsx
+++ src/components/ItemDetailsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { WynncraftItem } from '../types.js';
 import { getRarityColor } from '../utils/filterUtils.js';
 import './ItemDetailsModal.css';
